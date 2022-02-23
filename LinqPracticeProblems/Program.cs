11c02ed1049a837ea17b81af9d419e44b0a0336f
rasipriyanka <priyankapendela111@gmail.com>
2022-02-23 16:21:54 +0530

    [Priyanka]Added the program files

---
+++ LinqPracticeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linq!");
+        }
+    }
+}
